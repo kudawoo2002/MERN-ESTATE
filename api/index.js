7af8d49adf92e8ca4b6144330ca6d6e115e943dd
Kwami Kudawoo <kudawoo2002@gmail.com>
2026-05-29 15:40:20 +0000

    conect to mongo DB

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.mongoDbConnection)
+  .then(() => {
+    console.log("MongoDB Server connected Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const PORT = 3000;
